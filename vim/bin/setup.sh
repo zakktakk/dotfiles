be553e99e63154f6b64b551e8c45f1390238fcf8
Takuro Yamazaki <takuro.yamazaki.w@gmail.com>
2022-12-28 22:40:13 +0900

    update setup file

--- vim/bin/setup.sh
+++ vim/bin/setup.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# link vimrc
+mkdir -p $HOME/.vim/colors
 ln -s $HOME/dotfiles/vim/config.d/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/vim/config.d/hybrid.vim $HOME/.vim/colors/hybrid.vimrc
 
