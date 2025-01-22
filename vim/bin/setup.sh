9d5608392f491607820aeff4ae8f45490b87a76b
村山卓朗 <murayamatakuro@murayamatakurounoMacBook-Air-2.local>
2025-01-22 22:01:00 +0900

    fix vim setup sh

--- vim/bin/setup.sh
+++ vim/bin/setup.sh
@@ -5,6 +5,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # link vimrc
 mkdir -p $HOME/.vim/colors
 ln -s $HOME/dotfiles/vim/config.d/vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/vim/config.d/hybrid.vim $HOME/.vim/colors/hybrid.vimrc
+ln -s $HOME/dotfiles/vim/config.d/hybrid.vim $HOME/.vim/colors/hybrid.vim
 
 vim +PluginInstall +qall
